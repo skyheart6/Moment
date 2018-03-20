6d9819a0b3e3caa6b78344caa200245aa07d5f67
skyheart6 <tommygrz@gmail.com>
2018-03-20 03:20:09 -0400

    Pause Menu with working Resume Button

---
+++ Moment/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject pauseMenuUI;
+
+
+    public static bool gameIsPaused = false;
+
+
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+	}
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void Options()
+    {
+
+    }
+}
--- Moment/Assets/Scripts/PlayerController.cs
+++ Moment/Assets/Scripts/PlayerController.cs
@@ -176,6 +176,7 @@ public class PlayerController : MonoBehaviour {
             timeForm = true;
             shifted = true;
         }
+        
     }
 
 }
