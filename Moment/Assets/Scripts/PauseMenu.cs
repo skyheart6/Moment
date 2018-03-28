f6a69c75d9abaacd9201fa59d1094d7de6aff780
skyheart6 <tommygrz@gmail.com>
2018-03-28 01:25:51 -0400

    Test Level almost complete. Menu works now.

--- Moment/Assets/Scripts/PauseMenu.cs
+++ Moment/Assets/Scripts/PauseMenu.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour {
     public GameObject pauseMenuUI;
-
+    public GameObject controlMenuUI;
 
     public static bool gameIsPaused = false;
-
+    public static bool inControlMenu = false;
 
     void Update () {
-		if (Input.GetKeyDown(KeyCode.Escape))
+		if (Input.GetKeyDown(KeyCode.Escape) && !inControlMenu)
         {
             if (gameIsPaused)
                 Resume();
             else
                 Pause();
         }
+        else if (Input.GetKeyDown(KeyCode.Escape) && inControlMenu)
+        {
+            ExitFromOptions();
+        }
 
-	}
+    }
 
     public void Resume()
     {
@@ -36,7 +40,16 @@ public class PauseMenu : MonoBehaviour {
 
     public void Options()
     {
+        pauseMenuUI.SetActive(false);
+        controlMenuUI.SetActive(true);
+        inControlMenu = true;
+    }
 
+    public void ExitFromOptions()
+    {
+        pauseMenuUI.SetActive(true);
+        controlMenuUI.SetActive(false);
+        inControlMenu = false;
     }
 
     public void QuitGame()
